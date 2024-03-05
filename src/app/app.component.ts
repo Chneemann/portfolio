1db7e88302506192265ffa8bb47602b82e197468
Chneemann <Andre-Ke@gmx.de>
2024-03-05 16:59:11 +0100

    design navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
@@ -8,7 +8,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'Portfolio';
