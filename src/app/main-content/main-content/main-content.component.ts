d74ab22697cdb5e7723a8f787eb8a2349b7e8920
Chneemann <Andre-Ke@gmx.de>
2024-03-09 17:06:19 +0100

    design contact component

--- src/app/main-content/main-content/main-content.component.ts
+++ src/app/main-content/main-content/main-content.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
@@ -12,6 +13,7 @@ import { PortfolioComponent } from '../portfolio/portfolio.component';
     AboutMeComponent,
     SkillsComponent,
     PortfolioComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
