46c52057c849d8cd8b517e7d12af8a174db55e25
Chneemann <Andre-Ke@gmx.de>
2024-03-09 12:10:09 +0100

    added portfolio-single component & images

---
+++ src/app/main-content/portfolio/portfolio-single/portfolio-single.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-single',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-single.component.html',
+  styleUrl: './portfolio-single.component.scss'
+})
+export class PortfolioSingleComponent {
+
+}
