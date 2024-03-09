9c0128140ebfb5af5d83c43b25392cb71c601179
Chneemann <Andre-Ke@gmx.de>
2024-03-09 11:59:14 +0100

    added portfolio component

--- src/app/main-content/main-content/main-content.component.ts
+++ src/app/main-content/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, SkillsComponent],
+  imports: [
+    HeaderComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
