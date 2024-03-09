25b27f259b1f36ed060ec03423f3ca0c9b7bd775
Chneemann <Andre-Ke@gmx.de>
2024-03-09 13:15:05 +0100

    added data & design

--- src/app/main-content/portfolio/portfolio-single/portfolio-single.component.ts
+++ src/app/main-content/portfolio/portfolio-single/portfolio-single.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-single',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio-single.component.html',
-  styleUrl: './portfolio-single.component.scss'
+  styleUrl: './portfolio-single.component.scss',
 })
 export class PortfolioSingleComponent {
+  projects = [
+    {
+      name: 'Sharkie',
+      imgPath: 'sharkie.svg',
+      code: 'JavaScript | HTML | CSS',
+      description:
+        'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      linkLiveTest: '',
+      linkGithub: '',
+    },
+    {
+      name: 'Join',
+      imgPath: 'join.svg',
+      code: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      linkLiveTest: '',
+      linkGithub: '',
+    },
+  ];
 
+  getProjectStyle(i: number) {
+    return {
+      transform: i % 2 !== 0 ? 'scaleX(-1)' : '',
+    };
+  }
+
+  getDescriptionStyle(i: number) {
+    return {
+      transform: i % 2 !== 0 ? 'scaleX(-1)' : '',
+      'text-align': i % 2 !== 0 ? 'end' : '',
+    };
+  }
 }
