2281a65aaca7431e9c46ad916bd5de5bcb45fb7f
Chneemann <Andre-Ke@gmx.de>
2024-03-10 09:40:56 +0100

    design navbar links

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  activeLink: string | null | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment) => {
+      this.activeLink = fragment;
+    });
+  }
+
+  getLeftPosition() {
+    switch (this.activeLink) {
+      case 'about-me':
+        return '41px';
+      case 'skills':
+        return '184px';
+      case 'portfolio':
+        return '265px';
+      default:
+        return 'initial';
+    }
+  }
+}
