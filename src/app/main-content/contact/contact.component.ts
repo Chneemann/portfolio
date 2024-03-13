c8f5ceee978359dc6360f9d473d5a66a096632c1
Chneemann <Andre-Ke@gmx.de>
2024-03-13 10:36:26 +0100

    added form component

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
---
+++ src/app/main-content/contact/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent {}
