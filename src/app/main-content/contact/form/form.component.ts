96396583e53996a28bb5cab62625549c0e5870cc
Chneemann <Andre-Ke@gmx.de>
2024-03-13 10:55:09 +0100

    added ngModel

--- src/app/main-content/contact/form/form.component.ts
+++ src/app/main-content/contact/form/form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  contactData = {
+    name: '',
+    email: '',
+    msg: '',
+  };
+
+  onSubmit() {
+    console.log(this.contactData);
+  }
+}
