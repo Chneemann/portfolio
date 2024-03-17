fd47025a082f2bdd916383c8df8fb32f14115b4c
Chneemann <Andre-Ke@gmx.de>
2024-03-17 11:05:26 +0100

    added animation

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
+import { AnimationService } from '../../services/animation.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,6 +11,32 @@ import { LanguageService } from '../../services/language.service';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-  constructor(public langService: LanguageService) {}
+export class LandingPageComponent implements AfterViewInit {
+  constructor(
+    private elementRef: ElementRef,
+    private animationService: AnimationService,
+    public langService: LanguageService
+  ) {}
+
+  ngAfterViewInit() {
+    this.animationService.observeIntersection(
+      'left-text',
+      'animation-coming-in-iam',
+      this.elementRef
+    );
+    this.animationService.observeIntersection(
+      'name',
+      'animation-coming-in-name',
+      this.elementRef
+    );
+    this.animationService.observeIntersection(
+      'profession',
+      'animation-coming-in-profession',
+      this.elementRef
+    );
+  }
+
+  ngOnDestroy() {
+    this.animationService.disconnectObserver();
+  }
 }
