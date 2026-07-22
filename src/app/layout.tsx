141bfebc17ef07eec4909c53f31f7289a23919e5
Chneemann <andre-kempf@mailbox.org>
2026-07-22 08:17:31 +0200

    feat: add footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import Footer from "@/components/Footer";
 import Background from "@/components/Background";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Background />
         <Header />
         <main className="grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full border-t border-slate-900 bg-slate-950 text-center text-xs text-slate-500 px-4 sm:px-4 py-4">
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
+        <p>© {new Date().getFullYear()} André Kempf. All rights reserved.</p>
+        <div className="flex gap-4">
+          <Link
+            href="/imprint"
+            className="hover:text-slate-300 transition-colors"
+          >
+            Impressum
+          </Link>
+          <span>|</span>
+          <Link
+            href="/privacy"
+            className="hover:text-slate-300 transition-colors"
+          >
+            Datenschutz
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
