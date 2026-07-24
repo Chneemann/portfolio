3b3521ca918e9f5be0724d526ee97af3fd0147a1
Chneemann <andre-kempf@mailbox.org>
2026-07-24 13:04:38 +0200

    feat: add contact section

---
+++ src/components/Contact.tsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <section
+      id="contactme"
+      className="relative py-6 px-4 md:px-6 max-w-5xl mx-auto scroll-mt-15"
+    >
+      {/* Title */}
+      <div className="mb-6 space-y-1 text-right flex flex-col items-end">
+        <p className="text-xs font-mono text-blue-500 tracking-wider uppercase">
+          // 03. Get in Touch
+        </p>
+        <h2 className="text-3xl font-bold text-white tracking-tight">
+          Contact me<span className="text-blue-500">.</span>
+        </h2>
+      </div>
+    </section>
+  );
+}
